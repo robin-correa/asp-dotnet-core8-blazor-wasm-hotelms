24e29c52c2f04f20a93de9f86850d1d55bf47200
robin.correa <robin.correa21@gmail.com>
2024-05-12 02:43:48 +0800

    First commit.

---
+++ asp-dotnet-core8-blazor-wasm-hotelms/Program.cs
@@ -0,0 +1,11 @@
+using asp_dotnet_core8_blazor_wasm_hotelms;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
